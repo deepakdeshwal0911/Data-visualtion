db4155d00447d03d14be1edc6a4a949448190fbe
deepakdeshwal0911 <154494098+deepakdeshwal0911@users.noreply.github.com>
2026-02-24 11:01:39 +0530

    Add files via upload

---
+++ 100 Program/Grade assign.py
@@ -0,0 +1,28 @@
+# Assign grades based on marks
+
+marks = float(input())
+
+if marks >= 90:
+    print("A")
+elif marks >= 75:
+    print("B")
+elif marks >= 60:
+    print("C")
+elif marks >= 40:
+    print("D")
+else:
+    print("F")
+
+
+#Output:
+#Enter marks: 95
+#A
+#Enter marks: 80
+#B
+#Enter marks: 65
+#C
+#Enter marks: 50
+#D
+#Enter marks: 30
+#F
+
