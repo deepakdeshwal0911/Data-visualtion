3b8784dab84afe7a06530fd594e1cb57f17368af
deepakdeshwal0911 <154494098+deepakdeshwal0911@users.noreply.github.com>
2026-02-24 12:09:55 +0530

    Add files via upload

---
+++ 100 Program/Reversea sring with loop.py
@@ -0,0 +1,14 @@
+# Reverse a string using for loop
+
+text = input()
+
+reversed_text = ""
+
+for i in range(len(text) - 1, -1, -1):
+    reversed_text += text[i]
+
+print(reversed_text)
+
+#Output:
+#Enter a string: Hello World
+#dlroW olleH
