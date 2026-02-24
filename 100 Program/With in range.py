4f13b952848e15e98837e26ccecb689ed826063e
deepakdeshwal0911 <154494098+deepakdeshwal0911@users.noreply.github.com>
2026-02-24 11:22:29 +0530

    Add files via upload

---
+++ 100 Program/With in range.py
@@ -0,0 +1,28 @@
+
+
+num = float(input())
+lower = float(input())
+upper = float(input())
+
+if lower <= num <= upper:
+    print("Within Range")
+else:
+    print("Out of Range")
+
+
+#Output:
+#Enter a number: 5
+#Enter lower bound: 1
+#Enter upper bound: 10
+#Within Range
+#Enter a number: 0
+#Enter lower bound: 1
+#Enter upper bound: 10
+#Out of Range
+#Enter a number: 15
+#Enter lower bound: 1
+#Enter upper bound: 10
+#Out of Range
+
+
+
