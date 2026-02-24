a1d1d8c14e837d07b2e1bb6341007c09419354bf
deepakdeshwal0911 <154494098+deepakdeshwal0911@users.noreply.github.com>
2026-02-24 12:07:40 +0530

    Add files via upload

---
+++ 100 Program/Fibo with loop.py
@@ -0,0 +1,18 @@
+# Generate Fibonacci series using for loop
+
+n = int(input())
+
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=" ")
+    c = a + b
+    a = b
+    b = c
+
+
+#Output:
+#Enter the number of terms: 10
+#0 1 1 2 3 5 8 13 21 34
+
